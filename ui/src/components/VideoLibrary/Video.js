f05bdf4ef6c7e84ca78725dacca64e16f5fb217f
Chris Elston <chris.elston@blueyonder.co.uk>
2022-07-14 08:02:04 +0100

    show basic video detail

--- ui/src/components/VideoLibrary/Video.js
+++ ui/src/components/VideoLibrary/Video.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Video.css';
+
+const VideoMedium = ({ medium }) => {
+  return <p>{medium.format}{medium.quality && ` (${medium.quality})`}</p>
+};
 
 const Video = ({ video }) => {
   return (
     <div className='video-item'>
-      Video: {video.title} ({video.id})
+      <div className='video-title'>
+        {video.title}
+      </div>
+      <div className='video-type'>
+        {video.type}
+      </div>
+      <div className='video-media'>
+        {video.media.map((medium) => <VideoMedium key={Math.random} medium={medium} />)}
+      </div>
     </div>
   )
 };
